fb4151cf0d795efd3ad970695b479433d5b48443
IzykPringle <ijpringl@mtu.edu>
2023-03-14 15:50:16 -0600

    refactor

--- server.js
+++ server.js
@@ -1,9 +1,3 @@
 import http from 'http';
-const host = "localhost";
-const port = 3001;
-const server = http.createServer((request, response) => {
-    response.end("Hello, Galvanize!");
-});
-server.listen(port, host, () => {
-    console.log(`Server running at http://${host}:${port}/`);
-});
+const host = "localhost", port = 3001, server = http.createServer((request, response) => {response.end("Hello, Galvanize!") });
+server.listen(port, host, () => {console.log(`Server running at http://${host}:${port}/`)});
