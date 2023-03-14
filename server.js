5374b94f0f9827f2746c79871725ac4e8147daef
IzykPringle <ijpringl@mtu.edu>
2023-03-14 09:28:55 -0600

    create server

---
+++ server.js
@@ -0,0 +1,9 @@
+import http from 'http';
+const host = "localhost";
+const port = 3001;
+const server = http.createServer((request, response) => {
+    response.end("Hello, Galvanize!");
+});
+server.listen(port, host, () => {
+    console.log(`Server running at http://${host}:${port}/`);
+});
